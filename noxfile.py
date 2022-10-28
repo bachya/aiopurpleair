bad3c4a7b780a790bf6c0ac3a5dfb9295e790c14
Aaron Bach <bachya1208@gmail.com>
2022-10-27 18:26:07 -0600

    Add a variety of new GitHub Actions (#3)
    * Add a variety of new GitHub Actions
    
    * Try again
    
    * Again
    
    * Go
    
    * GO
    
    * AGAIN
    
    * aag
    
    * ASD
    
    * JKAHSD
    
    * ASDLj
    
    * KHASD
    
    * JKHASDASD
    
    * AGAIN
    
    * GO
    
    * GO

--- noxfile.py
+++ noxfile.py
@@ -1,28 +1,14 @@
 """Define nox actions."""
-import tempfile
-from typing import Any
-
 import nox
-
-
-def install_with_constraints(session: nox.sessions.Session, *args: str, **kwargs: Any):
-    """Install PyPI packages based on pyproject.toml constraints."""
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--without-hashes",
-            "--format=requirements.txt",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session
 def coverage(session: nox.sessions.Session) -> None:
-    """Report test coverage."""
+    """Report test coverage.
+
+    Args:
+        session: A nox session.
+    """
     args = session.posargs or [
         "-s",
         "--cov=aiopurpleair",
@@ -30,17 +16,19 @@ def coverage(session: nox.sessions.Session) -> None:
         "--cov-report=xml",
         "tests/",
     ]
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(
-        session, "aresponses", "pytest", "pytest-aiohttp", "pytest-cov"
-    )
+    session.run("poetry", "lock", external=True)
+    session.run("poetry", "install", external=True)
     session.run("pytest", *args)
 
 
 @nox.session
 def tests(session: nox.sessions.Session) -> None:
-    """Run all tests."""
+    """Run all tests.
+
+    Args:
+        session: A nox session.
+    """
     args = session.posargs or ["-s", "tests/"]
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(session, "aresponses", "pytest", "pytest-aiohttp")
+    session.run("poetry", "lock", external=True)
+    session.run("poetry", "install", external=True)
     session.run("pytest", *args)
