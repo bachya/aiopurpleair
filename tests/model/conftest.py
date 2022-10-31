0050d59c2e4c2f0789160edf41236f7d54a99ebc
Aaron Bach <bachya1208@gmail.com>
2022-10-30 21:41:15 -0600

    Fix conftest

--- tests/model/conftest.py
+++ tests/model/conftest.py
@@ -9,7 +9,11 @@ from tests.common import load_fixture
 
 @pytest.fixture(name="error_invalid_api_key_response", scope="session")
 def error_invalid_api_key_response_fixture() -> dict[str, Any]:
-    """Define a fixture for error response data from GET /keys."""
+    """Define a fixture for error response data from GET /keys.
+
+    Args:
+        Define an API response payload.
+    """
     return cast(
         dict[str, Any], json.loads(load_fixture("error_invalid_api_key_response.json"))
     )
@@ -17,5 +21,9 @@ def error_invalid_api_key_response_fixture() -> dict[str, Any]:
 
 @pytest.fixture(name="get_keys_response", scope="session")
 def get_keys_response_fixture() -> dict[str, Any]:
-    """Define a fixture for successful response data from GET /keys."""
+    """Define a fixture for successful response data from GET /keys.
+
+    Args:
+        Define an API response payload.
+    """
     return cast(dict[str, Any], json.loads(load_fixture("get_keys_response.json")))
